27d7c5efadbef8a0e3b1b91dd64746ecf8549d7e
Taghurei <36820181+Taghurei@users.noreply.github.com>
2018-10-19 16:01:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def index():
 def envoi():
     inputted_json = request.get_json(force=True) #On recupère le JSON envoyé par le parser linkedIn
     Liste_tag_garde=[] # On définit la liste des tags qu'on gardera
+    word_tokens=[]
     tag=["Java","C++","CentraleSupélec","Master","Paris","Nantes","centraledigitallab","Gauthier Lacroix","engineer","internship","student","technology","Developer","Agile","Scrum","Prototyper"]    
     for k in range (len(tag)):
         tag[k] = unidecode.unidecode(tag[k].lower()) #On enlève les caractères speciaux et les majuscules de notre liste de tag
@@ -26,17 +27,17 @@ def envoi():
         if (type(value) is dict):#et du dictionnaires de dictionnaires (boucle double for obligatoire)
             for key,value in value.items():
                 if (value):
-                    word_tokens = (unidecode.unidecode(value.lower())).split()#recuperation des donnees du dictionnaire ainsi que la position de la cle
-                    filtered_sentence = [w for w in word_tokens if not w in stop_words] #parcourt des mots des strings
-                    for w in word_tokens: 
-                        if w not in stop_words: #recuperation des mots utiles
-                            filtered_sentence.append(w) 
-                    liste_mot_utile.append(list(set(filtered_sentence)))
-    for i in (liste_mot_utile): #on parcourt la liste de mot et la liste de tag
-        for j in (i):
-            for k in tag:
-                if (j==k): # En cas d'égalité on ajoute le tag dans la liste des tag ressortis
-                    Liste_tag_garde.append(k)
+                 for k in ((unidecode.unidecode(value)).split(' ')): #On separe chaque groupe de mots obtenus pour n'avoir que des mots simples
+                    for z in k.split(','):
+                        word_tokens.append(z.lower()) #on ajoute les mots obtenus a notre liste de mots
+                filtered_sentence = [w for w in word_tokens if not w in stop_words] #parcourt des mots des strings
+    print(filtered_sentence)
+    print(tag)
+    for i in (filtered_sentence): #on parcourt la liste de mot et la liste de tag
+        for k in tag:
+            if (i==k): # En cas d'égalité on ajoute le tag dans la liste des tag ressortis
+                print("er")
+                Liste_tag_garde.append(k)
     return 'Tag ressorti: %s' % list(set(Liste_tag_garde)) # On renvoie dans la console les tags ressortis
 
 if __name__ == '__main__':
